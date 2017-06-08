52897e6f69858f160ad2a516d830bd41bbb1db9f
Stephen Wu <wujstep@gmail.com>
2017-06-08 13:56:11 -0400

    added real-time note tracking in midi/notes

--- demo/midi/midi.js
+++ demo/midi/midi.js
@@ -20,6 +20,12 @@ app.get('/player', (req, res) => {
   	});
 });
 
+app.get('/notes', (req, res) => {
+	res.render('notes.hbs', {
+		pageTitle: 'Notes Played'
+	});
+});
+
 var currentNote = -1;
 
 app.get('/note/:note', (req, res) => { // TODO: Also add allowing user to do /note/A0 instead of /0, etc.
@@ -49,12 +55,14 @@ app.get('/note', (req, res) => { // Give client incremented note
 const d3 = require('d3');
 const fs = require("fs");
 
-var notes = new Array(88); // Keep track of time at which notes are played
+var activeNotes = new Array(88); // Keep track of time at which notes are played
+const notesDefault = [{key: "A", duration: 0}, {key: "Bb", duration: 0}, {key: "B", duration: 0}, {key: "C", duration: 0}, {key: "Db", duration: 0}, {key: "D", duration: 0}, 
+		{key: "Eb", duration: 0}, {key: "E", duration: 0}, {key: "F", duration: 0}, {key: "Gb", duration: 0}, {key: "G", duration: 0}, {key: "Ab", duration: 0}];
 
 module.exports = {
 	_data: {
 		scheme: 0, // 0 = notes disappear immediately, 1 = notes stay fade
-		notes: new Array(88)
+		notes: JSON.parse(JSON.stringify(notesDefault)) // Copy notes array
 	},
 	
 	_generateOnServer: true, // Required, no default
@@ -69,6 +77,11 @@ module.exports = {
 			emit: "noteOff",
 			emitSender: false,
 			emitBroadcast: true,
+		},
+		reset: {
+			emit: "get data",
+			emitSender: true,
+			emitBroadcast: true
 		}
 	},
 	
@@ -77,17 +90,25 @@ module.exports = {
 	},
 	
 	noteOn: (params, callback) => { // TODO: Possibly validate input?
-		if (notes[params.note] === undefined || notes[params.note] === -1) { // Check that note is currently not already played
-			notes[params.note] = process.uptime();
+		const adjNote = params.note - 21;
+		if (activeNotes[adjNote] === undefined || activeNotes[adjNote] === -1) { // Check that note is currently not already played
+			activeNotes[adjNote] = process.uptime();
 		}
 		return callback(null, params);
 	},
 	
-	noteOff: (params, callback) => { 
-		if (notes[params.note] !== undefined && notes[params.note] !== -1) {
-			notes[params.note] = -1;
-			module.exports._data.notes[params.note] += process.uptime() - notes[params.note];
+	noteOff: (params, callback) => { // If C1 and C2 are being played, double count them.
+		const adjNote = params.note - 21;
+		const key = adjNote % 12;
+		if (activeNotes[adjNote] !== undefined && activeNotes[adjNote] !== -1) {
+			module.exports._data.notes[key].duration += process.uptime() - activeNotes[adjNote];
+			activeNotes[adjNote] = -1;
 		}
 		return callback(null, params);
+	},
+
+	reset: (params, callback) => {
+		module.exports._data.notes = JSON.parse(JSON.stringify(notesDefault)); // Copy default
+		callback(null, module.exports._data);
 	}
 };
