89417b7d8743504c3bdb4783b08dc97d7521d70e
Stephen Wu <wujstep@gmail.com>
2017-06-09 17:59:29 -0400

    404 + serving client files

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ var sockets = require('./sockets');
 var server = http.createServer(app);
 
 app.use(express.static(__dirname + "/../" + process.env.client));
+app.use("/client", express.static(__dirname + "/../client/"));
+
+app.use(function (req, res, next) {
+	res.status(404).send("404 - Sorry can't find that!")
+})
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
